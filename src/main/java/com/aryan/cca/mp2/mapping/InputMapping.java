92c5715764f2bca8fab32292213021b8a3c5419e
Aryan Kukreja <arysk@amazon.com>
2023-02-04 15:24:04 -0500

    Initial commit

---
+++ src/main/java/com/aryan/cca/mp2/mapping/InputMapping.java
@@ -0,0 +1,13 @@
+package com.aryan.cca.mp2.mapping;
+
+public class InputMapping {
+    private int num;
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
---
+++ src/test/java/com/aryan/cca/mp2/Mp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.cca.mp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
