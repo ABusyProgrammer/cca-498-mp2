72daa642fcdb3dd27595d153d0e248b43629b8a4
ABusyProgrammer <aryan.s.kukreja@gmail.com>
2023-02-04 21:36:54 -0500

    Update endpoint to match assignment requirements

--- src/main/java/com/aryan/cca/mp2/controllers/MainController.java
+++ src/main/java/com/aryan/cca/mp2/controllers/MainController.java
@@ -1,29 +1,27 @@
 package com.aryan.cca.mp2.controllers;
 
+import com.aryan.cca.mp2.mapping.InputMapping;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aryan.cca.mp2.mapping.InputMapping;
-
 @RestController
-@RequestMapping("hello-world/")
 public class MainController {
-    public static int num = 0;
+    public static Integer num = 0;
     
     @GetMapping("/")
     public ResponseEntity<Integer> get() {
-        return ResponseEntity.ok(Integer.valueOf(num));
+        return ResponseEntity.ok(num);
     }
 
     @PostMapping("/")
     public ResponseEntity<Integer> post(@RequestBody InputMapping input) {
         num = input.getNum();
 
-        return ResponseEntity.ok(Integer.valueOf(num));
+        return ResponseEntity.ok(num);
     }
     
 }
